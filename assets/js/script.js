3f24021970a1280ac4e60a6915e95d9139ebee76
abbygraves <abbygraves14@gmail.com>
2022-05-27 07:30:56 -0500

    working on getting questions and choices to show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,35 @@
 var questions = [
   {
     question: "Commonly used data types do NOT include:",
-    answer1: "alerts",
-    answer2: "booleans",
-    answer3: "strings",
-    answer4: "numbers",
-    correct: "alerts"  //answer1
+    choices: ["alerts", "booleans", "strings", "numbers"],
+    answer: "alerts" 
   },
   {
     question: "The condition in an if / else statement is enclosed with _______.",
-    answer1: "quotes",
-    answer2: "curly brackets",
-    answer3: "parenthesis",
-    answer4: "square brackets",
-    correct: "parenthesis"  //answer3
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
   },
   {
     question: "Arrays in JavaScript can be used to store _______.",
-    answer1: "numbers and strings",
-    answer2: "other arrays",
-    answer3: "booleans",
-    answer4: "all of the above", 
-    correct: "all of the above"  //answer4
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
   },
   {
     question: "String values must be enclosed within _______ when being assigned to variables.",
-    answer1: "commas",
-    answer2: "curly brackets",
-    answer3: "quotes",
-    answer4: "parenthesis",
-    correct: "quotes"  //answer3
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
   },
   {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    answer1: "JavaScript",
-    answer2: "terminal/bash",
-    answer3: "for loops",
-    answer4: "console.log",
-    correct: "console.log"  //answer4
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log" 
   }
 ];
 
 var startBtn = document.getElementById("start-btn");
 var mainPage = document.getElementById("main-page");
 var questionEl = document.querySelector(".questions");
+var choicesEl = document.querySelector(".choices")
 
 function startQuiz() {
   startBtn.style.display = 'none';
@@ -51,8 +37,6 @@ function startQuiz() {
 
   var questionBox = document.getElementById("question-box");
   questionBox.style.display = 'block';
-
-  
 
   // setInterval function
   // if timer runs out and i have not completed questions then do this 
@@ -63,12 +47,27 @@ function startQuiz() {
 
 
 function getQuestions() {
-  questionEl.style.display = 'block';
-  var nextQuestion = questions[0];
+  for (i = 0; i < questions.length; i++) {
+    console.log("Element at index " + i + " : "+ questions[i]);
 
-  var questionTitleEl = document.getElementById("question-title");
-  questionTitleEl.innerHTML = "<h1 class='title'>" + nextQuestion.question + "</h1>";
-  console.log(questionTitleEl.innerHTML);
+    questionEl.style.display = 'block';
+    var newQuestion = questions[i];
+    console.log(newQuestion)
+    
+    var questionTitleEl = document.getElementById("question-title");
+    questionTitleEl.innerHTML = "<h1 class='title'>" + newQuestion.question + "</h1>";
+    //console.log(questionTitleEl.innerHTML);
+  }
 };
 
+
+function getChoices() {
+     var newChoices = questions[0]
+
+     var buttonTextEl = document.getElementById("choice-1-btn");
+     buttonTextEl.textContent = newChoices;
+     console.log(newChoices)
+};
+
+//getChoices();
 startBtn.addEventListener("click", startQuiz);
